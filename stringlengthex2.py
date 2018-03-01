8cb98b874cc6109c824a8fe8c004b5d062a24568
mdzapeer <36439677+mdzapeer@users.noreply.github.com>
2018-03-01 23:14:53 +0400

    fixed on fuction

--- stringlengthex2.py
+++ stringlengthex2.py
@@ -2,8 +2,7 @@ def stringlength(stringvar):
     if type(strinput) == str:
         return len(stringvar)
     else:
-        print("this is not a string")
-
+        return "this is not a string"
 
 strinput=int(input("Enter a string: "))
 print (stringlength(strinput))
