b0cc66da4d24fa674e1424d70cc14e1ed19bc4f6
mdzapeer <36439677+mdzapeer@users.noreply.github.com>
2018-03-03 19:13:04 +0400

    a

--- celsiustoFahrenheit.py
+++ celsiustoFahrenheit.py
@@ -1,6 +1,9 @@
 def ctof(cel):
-        fahren=cel*(9/5)+32
-        return  fahren
+        if cel >= -273.15:
+            fahren=cel*(9/5)+32
+            return fahren
+        else:
+            return ("Temperature lower than physically possible")
 
 celinput=input ("Enter Celsius to convert to Fahrenheit: ")
 print (ctof(float(celinput)))
