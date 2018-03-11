1deda05c850e507722ade3b1dfef4ed4152c8a0e
mdzapeer <36439677+mdzapeer@users.noreply.github.com>
2018-03-11 19:07:24 +0400

    1

--- Lecture72Ex/textfilemerge.py
+++ Lecture72Ex/textfilemerge.py
@@ -9,9 +9,10 @@ def writer (filename, whatwrite):
     with open (filename, "a+") as file2:
         file2.write(whatwrite)
 
-i=1
+
 currenttimeobj=datetime.now()
 currenttimestr=currenttimeobj.strftime("%Y-%m-%d-%h-%M-%S-%f")+".txt"
+i=1
 while i<4:
     contents=reader("file"+ str(i)+".txt")
     i=i+1
