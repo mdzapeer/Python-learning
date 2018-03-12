1a5d3040e67f31198e88ec8ccf52a2ae8f45fc31
mdzapeer <36439677+mdzapeer@users.noreply.github.com>
2018-03-12 18:58:20 +0400

    12

---
+++ Interactive Dictionary/DictionaryApp.py
@@ -0,0 +1,11 @@
+import json
+
+def extractj (key):
+    data=json.load(open("data.json"))
+    if key in data:
+        return (data[key])
+    else:
+        return ("Word not in dicitionary")
+
+userin=input ("Enter a word: ")
+print (extractj(userin))
