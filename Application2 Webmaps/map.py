a86858d0859cd112154c5ea2c5259eec783e4fcf
mdzapeer <36439677+mdzapeer@users.noreply.github.com>
2018-03-29 14:34:05 +0400

    116

--- Application2 Webmaps/map.py
+++ Application2 Webmaps/map.py
@@ -3,11 +3,20 @@ import pandas
 
 voldata=pandas.read_csv("Volcanoes_USA.txt")
 
+def elevcolor (elev):
+    if elev < 1000:
+        return 'green'
+    elif 1000<= elev <3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 map1=folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 fg1=folium.FeatureGroup(name="My Map")
 
-for lt,ln,name,loc in zip(voldata.LAT,voldata.LON,voldata.NAME, voldata.LOCATION):
-    fg1.add_child(folium.Marker(location = [lt, ln], popup=folium.Popup(name+" "+loc,parse_html=True), icon=folium.Icon(color='purple')))
+for lt,ln,name,loc,elv in zip(voldata.LAT,voldata.LON,voldata.NAME, voldata.LOCATION, voldata.ELEV):
+    fg1.add_child(folium.Marker(location = [lt, ln], popup=folium.Popup(name+" "+loc,parse_html=True), icon=folium.Icon(elevcolor(elv))))
 
 map1.add_child(fg1)
 
