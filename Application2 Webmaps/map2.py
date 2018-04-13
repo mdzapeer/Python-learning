07fea5459a7259612471015751cc3efe0c7ebdef
mdzapeer <36439677+mdzapeer@users.noreply.github.com>
2018-04-13 20:20:42 +0400

    123

--- Application2 Webmaps/map2.py
+++ Application2 Webmaps/map2.py
@@ -13,11 +13,17 @@ def elevcolor (elev):
 
 
 map1=folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
-fg1=folium.FeatureGroup(name="My Map")
+fg1=folium.FeatureGroup(name="Volcaonoes")
 
 for lt,ln,name,loc,elv in zip(voldata.LAT,voldata.LON,voldata.NAME, voldata.LOCATION, voldata.ELEV):
     fg1.add_child(folium.CircleMarker(location = [lt, ln], color=elevcolor(elv), fill='TRUE', popup=folium.Popup(name+" "+loc,parse_html=True)))
 
-map1.add_child(fg1)
+fg2=folium.FeatureGroup(name="Population")
+fg2.add_child(folium.GeoJson(data=open('world.json','r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] <= 10000000 else 'orange' if 10000000 < x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map1.save("map3.html")
+map1.add_child(fg1)
+map1.add_child(fg2)
+map1.add_child(folium.LayerControl())
+
+map1.save("map6.html")
