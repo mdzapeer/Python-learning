250f6b5eee44f68180c29aba780c4cce49573a77
mdzapeer <36439677+mdzapeer@users.noreply.github.com>
2018-04-16 02:06:24 +0400

    done!

--- App3 Website blocker/web_blocker.py
+++ App3 Website blocker/web_blocker.py
@@ -6,16 +6,26 @@ redirect="127.0.0.0"
 block_sites=['www.facebook', 'facebook.com', 'www.youtube.com','youtube.com']
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,21):
-        print("inside")
-        with open(filepath,'r+') as file:
-            content=file.read()
-            for website in block_sites:
-                if website in content:
-                    pass
-                else:
-                    file.write(redirect+' '+website+'\n')
-
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,20):
+            print("inside")
+            with open(filepath,'r+') as file:
+                content=file.read()
+                for website in block_sites:
+                    if website in content:
+                        pass
+                    else:
+                        file.write(redirect+' '+website+'\n')
     else:
-        print("outside")
+            print("outside")
+            with open(filepath,'r') as file:
+                content=file.read()
+                file.seek(0)
+                for line in file:
+                    if any(site in line for site in block_sites) == True:
+                        print('found: '+line)
+                        content=content.replace(line,"")
+                    else:
+                        print('not there')
+            with open(filepath,'w') as outputfile:
+                outputfile.write(str(content))
     time.sleep(5)
